a03e6cb35f4b2bdec912f192dc427e62178a53e5
Gladstone Rose <gladstonerose@hotmail.com>
2024-10-01 08:30:39 -0500

    modified files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+base: '/sucess-vite-version3/',
   plugins: [react()],
 })
